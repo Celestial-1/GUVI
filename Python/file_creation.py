5e687293c1922e03aa19f3cb64f77db4bffe8de7
Celestial-1 <yashrocks3053@gmail.com>
2023-12-27 17:24:43 +0530

    file creation

--- Python/file_creation.py
+++ Python/file_creation.py
@@ -1,5 +1,5 @@
 import os
-for i in range(4,25) :
+for i in range(6,25) :
     old_name=(f'GUVI\Python\Question_{i+1}')
     new_name=(f'GUVI\Python\Question_{i+1}.py')
     os.rename(old_name,new_name)
