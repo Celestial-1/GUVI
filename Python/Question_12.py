e3cd917e6bd04e708b7071136af6c6b0d3c60869
Celestial-1 <yashrocks3053@gmail.com>
2023-12-28 12:53:35 +0530

    Write a program to check whether a person is eligible for voting or not. If age is greater than or equal to 18 display 'yes' else 'no'

---
+++ Python/Question_12.py
@@ -0,0 +1,23 @@
+# Write a program to check whether a person is eligible for voting or not. If age is greater than or equal to 18 display “yes” else “no”
+
+# Input Description:
+# First line consists of input number
+
+# Output Description:
+# Output consists of “yes” or “no”
+
+# Sample Input :
+# 18
+# Sample Output :
+# yes
+
+
+
+# Get age as input
+age = int(input())
+
+# Check eligibility
+if age >= 18:
+    print("yes")
+else:
+    print("no")
